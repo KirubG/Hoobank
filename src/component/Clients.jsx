70416591445052276b53496b061f5bcd6a1a3793
Kirubel Girma <Kirub1896@gmail.com>
2025-03-10 23:13:34 +0300

    add a marquee to client section

--- src/component/Clients.jsx
+++ src/component/Clients.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
-import { clients } from '../constant'
+import React from "react";
+import { clients } from "../constant";
+import Marquee from "react-fast-marquee";
 
 const Clients = () => {
   return (
-    <div className='items-center w-full mt-20 flex justify-between gap-10'>
-        {clients.map((client, index)=>{
-            return (
-              <div key={client.id} className='flex items-center justify-center '>
-                <img src={client.logo} alt={client.id} width={200}/>
-              </div>
-            )
+    <div>
+      <Marquee
+        speed={50}
+        pauseOnHover={true}
+        className="items-center w-full mt-20 flex justify-between gap-10"
+      >
+        {clients.map((client, index) => {
+          return (
+            <div
+              key={client.id}
+              className="flex items-center justify-between mx-20 "
+            >
+              <img src={client.logo} alt={client.id} width={200} />
+            </div>
+          );
         })}
+      </Marquee>
+      <Marquee
+        direction="right"
+        speed={50}
+        pauseOnHover={true}
+        className="items-center w-full mt-20 flex justify-between gap-10"
+      >
+        {clients.map((client, index) => {
+          return (
+            <div
+              key={client.id}
+              className="flex items-center justify-between mx-20 "
+            >
+              <img src={client.logo} alt={client.id} width={200} />
+            </div>
+          );
+        })}
+      </Marquee>
     </div>
-  )
-}
+  );
+};
 
-export default Clients
+export default Clients;
